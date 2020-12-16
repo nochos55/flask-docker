beb4d22192b5f8bd5fead86f17cd44709ae610a5
rozari <rozari@amazon.com>
2020-12-15 22:57:31 -0800

    Adding configMap variable

--- web/app.py
+++ web/app.py
@@ -1,9 +1,9 @@
-
+import os
 from flask import Flask, render_template
- 
+
 app = Flask(__name__)
   
-   
+ 
 @app.route('/')
 def hello_whale():
     return render_template("index.html")
@@ -15,6 +15,8 @@ def test():
 @app.route('/health')
 def heathy():
     return 'I am Healthy!'
-
+@app.route('/cm')
+def cm():
+    return 'My value is ' + os.getenv('CM_VALUE')
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
