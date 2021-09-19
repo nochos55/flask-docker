ef5fb17c65ace07b1195514aa3df0e5ab42e505e
Anthony Rozario <Anthonyzilla94@gmail.com>
2021-09-18 20:17:54 -0700

    Modernizing project with gitops practices

--- web/app.py
+++ web/app.py
@@ -1,20 +1,49 @@
-
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify, make_response
+import logging 
+import json 
  
 app = Flask(__name__)
+
+logging.basicConfig(filename='logs/app.log', level=logging.DEBUG)
    
 @app.route('/')
 def hello_whale():
+    endpoint = "Home Page"
+    app.logger.info(f"{endpoint} endpoint was reached")
     return render_template("index.html")
 
 @app.route('/test')
 def test():
+    endpoint = "Testing Page"
+    app.logger.info(f"{endpoint} was reached")
     return render_template("test.html") 
 
-@app.route('/health')
-def heathy():
-    return 'I am Healthy!'
+@app.route('/status')
+def health_check():
+    endpoint = "Status"
+    #return make_response(jsonify(data), status)
+    
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status = 200,
+        mimetype='application/json'
+    )
+    app.logger.info(f"{endpoint} was reached")
+    return response
 
+@app.route('/metrics')
+def metrics():
+    data = {"UserCount" : 140, "UserCountActive" : 23}
+    endpoint = "Metrics"
+#   return make_response(jsonify(data), status)
+    response = app.response_class(
+        response=json.dumps({"status":"success","code":0, "data":data}),
+        status = 200,
+        mimetype='application/json'
+    )
+    app.logger.info(f"{endpoint} was reached")
+    return response
+    
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,port=5000)
     
